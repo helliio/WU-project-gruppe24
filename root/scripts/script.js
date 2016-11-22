954e724afae8f396ccd101045ea3a645e85e2207
helliio <liang150393@gmail.com>
2016-11-22 12:20:08 +0100

    increses ease in out tiem and height

--- root/scripts/script.js
+++ root/scripts/script.js
@@ -2,14 +2,14 @@ $(".list-group-item").click(function() {
 	if($(this).hasClass("t")){
 		var h = $(this).height() + 22
 		$(this).css({
-			//transition: 'height 0.2s ease-in-out',
-			"height": (h-100) + "px"	
+			transition: 'height 0.4s ease-in-out',
+			"height": (h-300) + "px"	
 		}).removeClass("t")
 	}else{
 		var h = $(this).height() + 22
 		$(this).css({
-			//transition: 'height 0.2s ease-in-out',
-			"height": (h+100) + "px"
+			transition: 'height 0.4s ease-in-out',
+			"height": (h+300) + "px"
 		}).addClass("t")
 	}
 });
