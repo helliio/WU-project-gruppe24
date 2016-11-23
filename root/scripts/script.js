8b8424feb1aeddc860d1635acd409cd74a96e67e
helliio <liang150393@gmail.com>
2016-11-23 12:16:39 +0100

    added background blue for debugging purpose (WIP)

--- root/scripts/script.js
+++ root/scripts/script.js
@@ -2,7 +2,7 @@ $(".list-group-item").click(function() {
 	if($(this).hasClass("toggled")){
 		$(this).css({
 			transition: 'height 0.4s ease-in-out',
-			"height": "100px"	
+			"height": "100px"
 		}).removeClass("toggled")
 	}else{
 		$(this).css({
