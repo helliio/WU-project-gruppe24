7c8393e7d54d8de9d132ceb07751f4779048a3c0
Magnus Bakke <magnusbakke@live.com>
2016-11-24 17:46:29 +0100

    Fixed cabin list item collapse and expand

--- root/scripts/script.js
+++ root/scripts/script.js
@@ -1,23 +1,25 @@
-$(".hytte-header-container").click(function() {
+$(".hytte-header-container").click(function(){
 	if($(this).hasClass("toggled")){
-		$(this).closest('.bestillingsknapper').css({
-			transition: 'opacity 0.4s ease-in-out',
-			"opacity": "0"
-		})
-
 		$(this).closest('.list-group-item').css({
 			transition: 'height 0.4s ease-in-out',
 			"height": "100px"
 		})
+		$('.bestillingsknapper', this.closest('.list-group-item')).css({
+			transition: 'opacity 0.4s ease-in-out',
+			"opacity": "0"
+		})
 		$('.hytte-toggle-arrow img', this).css({
 			transform: "scaleY(1)"
 		})
+		var objekttest = $('.bestillingsknapper', this.closest('.list-group-item'));
+		setTimeout(skjulObjekt.bind(null, objekttest), 500);
 		$(this).removeClass("toggled")
-	}else{
-		$(this).closest('.bestillingsknapper').css({
-			transition: 'opacity 0.4s ease-in-out',
-			"opacity": "1"
+	} else {
+		$('.bestillingsknapper', this.closest('.list-group-item')).css({
+			"display": "inline"
 		})
+		var objekttest = $('.bestillingsknapper', this.closest('.list-group-item'));
+		setTimeout(visObjekt.bind(null, objekttest), 400);
 
 		$(this).closest('.list-group-item').css({
 			transition: 'height 0.4s ease-in-out',
@@ -35,3 +37,16 @@ $('.thumbnail').click(function() {
 	$('.imagepreview').attr('src', img);
 	$('#imagemodal').modal('show');
 });
+
+function skjulObjekt(objekt){
+	objekt.css({
+		"display": "none",
+	});
+}
+
+function visObjekt(objekt){
+	objekt.css({
+		transition: 'opacity 0.4s ease-in-out',
+		"opacity": "1"
+	});
+}
