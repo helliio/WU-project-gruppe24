3f215d724d76c61c3ba1339971d9c8d38d738637
Magnus Bakke <magnusbakke@live.com>
2016-11-25 15:05:19 +0100

    PHP changes

--- root/bekreft.php
+++ root/bekreft.php
@@ -13,66 +13,97 @@
 </head>
 <body>
 	<a href="index.html">
-	<header class="jumbotron text-center">
-		<h1 class="logo">Fjell &amp; Fjord</h1>
-		<p>Hytteutleie for alle!</p>
-	</header>
+		<header class="jumbotron text-center">
+			<h1 class="logo">Fjell &amp; Fjord</h1>
+			<p>Hytteutleie for alle!</p>
+		</header>
 	</a>
+	<section>
+		<div class="panel-group">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<div class="panel-title">
+						<a data-toggle="collapse" href="#collapse1">
+							<span class="visible-xs">
+								<h2>Meny</h2>
+							</span>
+							<span class="visible-sm">
+								<h2>Meny</h2>
+							</span>
+							<span class="visible-md">
+								<h2>Meny</h2>
+							</span>
+							<span class="visible-lg">
+								<h2>Meny</h2>
+							</span>
+						</a>
+					</div>
+				</div>
+				<div id="collapse1" class="panel-collapse collapse">
+					<a href="index.html"><div class="panel-body">Forside</div></a>
+					<a href="omoss.html"><div class="panel-body">Om oss</div></a>
+					<a href="kontakt.html"><div class="panel-body">Kontakt oss</div></a>
+				</div>
+			</div>
+		</div>
+	</section>
 
 	<main>
 		<div class="section-separator">
 		</div>
 
 		<div class="bekreftelse">
-		<?php
+			<?php
 			$rs = $_GET['rs'];
-			$rs = $_GET['vn'];
-			$rs = $_GET['st'];
-		?>
-		<form>
-			<h1 id="bekreftelse-header">Bekreft bestilling</h1>
-			Fornavn og etternavn:
-			<input type="text" id="navn"name="navn" required="true">
-			E-post adresse:
-			<input type="email" id="epost" name="epost" min="0">
-			Telefonnummer:
-			<input type="text" id="tlf" name="tlf" required="true">
-			Adresse:
-			<input type="text" id="adresse" name="adresse" required="true">
+			$vn = $_GET['vn'];
+			$st = $_GET['st'];
+			$kt = $_GET['kt'];
+			echo "teeeeeeeest!" . $rs . $vn . $st . $kt;
+			?>
+			<form>
+				<h1 id="bekreftelse-header">Bekreft bestilling</h1>
+				Fornavn og etternavn:
+				<input type="text" id="navn"name="navn" required="true">
+				E-post adresse:
+				<input type="email" id="epost" name="epost" min="0">
+				Telefonnummer:
+				<input type="text" id="tlf" name="tlf" required="true">
+				Adresse:
+				<input type="text" id="adresse" name="adresse" required="true">
 
-			<div class="split-wrapper">
-				<input type="checkbox" name="vask" value="vask">
-				<div class="element-hoyre">
-					<div class="tekst-venstre bekreftelse-check">Jeg har ikke anledning til å vaske opp etter oppholdet (+500,-)</div>
+				<div class="split-wrapper">
+					<input type="checkbox" name="vask" value="vask">
+					<div class="element-hoyre">
+						<div class="tekst-venstre bekreftelse-check">Jeg har ikke anledning til å vaske opp etter oppholdet (+500,-)</div>
+					</div>
 				</div>
-			</div>
-			<div class="split-wrapper">
-				<input type="checkbox" name="turistforening" value="turistforening">
-				<div class="element-hoyre">
-					<div class="tekst-venstre bekreftelse-check">Jeg er medlem av turistforeningen (-20%)</div>
+				<div class="split-wrapper">
+					<input type="checkbox" name="turistforening" value="turistforening">
+					<div class="element-hoyre">
+						<div class="tekst-venstre bekreftelse-check">Jeg er medlem av turistforeningen (-20%)</div>
+					</div>
 				</div>
-			</div>
 
-			Ekstra kommentarer:
-			<textarea id="ekstraKommentar" name="sporsmal" form="usrform"></textarea>
-			Hvordan vil du bekrefte kjøpet?
-			<br/>
-			<input type="radio" name="sms" value="sms" checked>SMS
-			<input type="radio" name="epost" value="Epost" id="margin-left-10">Epost
-			<button type="submit" id="sendInn">Bekreft bestilling</button>
-		</form>
+				Ekstra kommentarer:
+				<textarea id="ekstraKommentar" name="sporsmal" form="usrform"></textarea>
+				Hvordan vil du bekrefte kjøpet?
+				<br/>
+				<input type="radio" name="sms" value="sms" checked>SMS
+				<input type="radio" name="epost" value="Epost" id="margin-left-10">Epost
+				<button type="submit" id="sendInn">Bekreft bestilling</button>
+			</form>
 		</div>
 
-	</section>
-	<div id="copyright">
-		<a href="index.html">Hjem</a><br>
-		<a href="omoss.html">Om oss</a><br>
-		<a href="kontakt.html">Kontakt oss</a><br>
-		<p>Copyright © Team 24 (Working Title)</p>
-	</div>
+		<section>
+			<div id="copyright">
+				<a href="index.html">Hjem</a><br>
+				<a href="omoss.html">Om oss</a><br>
+				<a href="kontakt.html">Kontakt oss</a><br>
+				<p>Copyright © Team 24 (Working Title)</p>
+			</div>
 
-</section>
-</main>
-<script src="scripts/script.js"></script>
+		</section>
+	</main>
+	<script src="scripts/script.js"></script>
 </body>
 </html>
